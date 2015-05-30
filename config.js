d769242a89986517b6cc40efedb560fb17d65ea5
JV Estolas <fortis201@gmail.com>
2015-05-30 11:50:35 -0700

    Pushing my edited starter kit

---
+++ config.js
@@ -0,0 +1,8 @@
+
+// This is a sample config.js file where you will put the tokens that you get from Uber
+// remember to change these AND change the name of the file to config.js after you are done
+module.exports = {
+  ClientID: "cpaG2FFmanKWaXuFgttDCefvdS0qBGG4",
+  ClientSecret: "T7dkixwyjUDf6oqImQ9xCYY1xn75hpH1u95qxFaS",
+  ServerID: "lbyXqB_lCr8okFviP_qd17Ba-DPiYA9Gbqy8O-F8"
+}
